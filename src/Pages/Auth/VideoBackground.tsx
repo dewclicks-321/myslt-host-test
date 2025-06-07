0d0a39b80c300de02088be6a934ecb0e2bb1e4d2
Risandu Dewmika <dewclicks99@gmail.com>
2025-06-07 21:36:13 +0530

    ss

--- src/Pages/Auth/VideoBackground.tsx
+++ src/Pages/Auth/VideoBackground.tsx
@@ -1,16 +1,13 @@
-// components/VideoBackground.tsx
 import React from 'react';
 import './VideoBackground.css';
-import backgroundVideo from '../../assets/background.mp4';
 
 const VideoBackground: React.FC = () => {
   return (
     <div className="video-background">
       <video autoPlay loop muted playsInline>
-        <source src={backgroundVideo} type="video/mp4" />
+        <source src="/background.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
-      {/* <div className="overlay"></div> */}
     </div>
   );
 };
